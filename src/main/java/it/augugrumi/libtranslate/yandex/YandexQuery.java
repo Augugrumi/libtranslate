0f96efd1263c01aafc6f467f0185aa001acb8f0a
mzanella <zanna0150@gmail.com>
2017-05-15 07:33:49 +0200

    Add comment on YandexQuery for the problem when the starting language is not known

--- src/main/java/it/augugrumi/libtranslate/yandex/YandexQuery.java
+++ src/main/java/it/augugrumi/libtranslate/yandex/YandexQuery.java
@@ -28,6 +28,37 @@ public class YandexQuery implements ITranslationQuery {
         this.textToTranslate = textToTranslate;
     }
 
+    /* TODO think about that problem
+    * What if we don't know which is the language from which we would like to translate?
+    * Yandex has the possibility to translate to a certain language even if the starting language is
+    * not known `api.translationApi().translate("Hola, mundo!", Language.FR).text()`
+    * Below a possible solution
+    *
+        String key = TranslateKeyStore.getInstance().retrieveYandexKey();
+        YTranslateApi yandexApi = new YTranslateApiImpl(key);
+        com.github.vbauer.yta.model.Language adaptedTo = YandexLangConverter.getConverter().get(to).convert();
+        com.github.vbauer.yta.model.Language adaptedFrom = null
+
+        Translation translation = null
+        if (from != null) {
+            adaptedFrom = YandexLangConverter.getConverter().get(from).convert();
+            translation = yandexApi.translationApi()
+                .translate(textToTranslate,Direction.of(adaptedFrom, adaptedTo));
+        } else {
+            translation = yandexApi.translationApi()
+                .translate(Direction.of(textToTranslate, adaptedTo));
+        }
+
+
+        YandexResult res = new YandexResult
+                .Builder()
+                .withTranslation(translation.text())
+                .build();
+
+        // TODO call the original library and do the query!
+        return res;
+    *
+    */
     @Override
     public ITranslationResult runQuery() {
 
